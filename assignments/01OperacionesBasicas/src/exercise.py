41e03d9a823027fdf0ebd62d3c82a23c3908de60
A01641178 <a01641178@itesm.mx>
2021-08-20 13:10:42 +0000

    envio 1

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
+    n1=int(input('Dame un número: '))
+    n2=int(input('Dame un número: '))
+    mult=n1*n2
+    suma=n1+n2
+    res =n1-n2
+    print('Suma: '+str(suma))
+    print('Resta: '+str(res))
+    print('Multiplicación: '+str(mult))
+
+
+
+
 
 
 
